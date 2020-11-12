624fdd3a23fec0f4a782adee31b7646f4238cfbb
Latesh-Kumari <latesh.kumari_cs19@gla.ac.in>
2020-11-12 22:20:10 +0530

    add images

--- js/clock.js
+++ js/clock.js
@@ -2,7 +2,11 @@
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
+const date = document.getElementById('date');
 //Event Handler
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+
 
 //Functions
 function showtime() {
@@ -10,9 +14,11 @@ function showtime() {
     let hour = today.getHours();
     let min = today.getMinutes();
     let sec = today.getSeconds();
+    let todaydate = today.toDateString();
     const amPm = hour > 12 ? 'PM' : 'AM';
     hour = hour % 12 || 12;
     time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${amPm}`;
+    date.innerHTML = `${todaydate}`;
     setTimeout(showtime, 1000);
 }
 
@@ -21,14 +27,50 @@ function addZero(n) {
 }
 
 function setGreeting() {
-    let today = new Date();
+    let today = new Date(2020, 11, 12, 10, 12, 10);
     let hour = today.getHours();
     if (hour < 12) {
-        document.body.style.backgroundImage = 'url("morning.jpg")';
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../img/morning.jpg")';
+        greeting.innerHTML = "Good Morning";
+        document.body.style.color = "white";
 
+    } else if (hour < 18) {
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../img/good.jpg")';
+        greeting.innerHTML = "Good Afternoon";
+
+    } else {
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../img/eve.jpg")';
+
+        greeting.innerHTML = "Good Evening";
+        document.body.style.color = "white";
+    }
+
+
+}
+
+function getName() {
+    if (localStorage.getItem("mykeyname") == null) {
+        name.innerHTML = '[Enter name]';
+    } else {
+        name.innerHTML = localStorage.getItem("mykeyname");
+    }
+}
+
+function setName(e) {
+    if (e.type === "keypress") {
+        if (e.keyCode == 13) {
+            localStorage.setItem("mykeyname", e.target.innerHTML);
+            name.blur();
+        }
+    } else {
+        localStorage.setItem("mykeyname", e.target.innerHTML);
     }
 }
 
 //Functions call
 showtime();
 setGreeting();
+getName();
