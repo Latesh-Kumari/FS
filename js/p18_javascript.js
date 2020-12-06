7c547e816e97915960a48ef5af9705ebe95111d9
Latesh Kumari <latesh.kumari_cs19@gla.ac.in>
2020-12-06 16:23:30 +0530

    completed practical-20

--- js/p18_javascript.js
+++ js/p18_javascript.js
@@ -1,87 +1,68 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password1');
+const password2 = document.getElementById('password2');
 
-const form= document.getElementById('form');
-const username= document.getElementById('username');
-const email= document.getElementById('email');
-const password1= document.getElementById('password1');
-const password2= document.getElementById('password2');
-const small= document.querySelector('small');
-
-form.addEventListener('submit',function (e) {
-  e.preventDefault();
-  checkInput();
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    checkInputs();
 });
 
-function checkInput()
-{
-    const usernameValue=username.value.trim();
-    const emailValue=email.value.trim();
-    const password1Value=password1.value.trim();
-    const password2Value=password2.value.trim();
-   if(usernameValue==='')
-   {
-     showError(username,"Username can not be blank");
-   }
-   else if(!isUsernameValid(usernameValue))
-   {
-       showError(username,"Username is not valid");
-   }
-   else
-   {
-       showSuccess(username);
-   }
-    if(emailValue==='')
-    {
-        showError(email,"Email can not be blank");
-    }
-    else if(!isEmailValid(emailValue))
-    {
-        showError(email,"Email ID is not valid");
-    }
-    else
-    {
-        showSuccess(email);
+function checkInputs() {
+    // trim to remove the whitespaces
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
+    if (usernameValue === '') {
+        setErrorFor(username, 'Username cannot be empty');
+    } else if (!isUsernameValid(usernameValue)) {
+        setErrorFor(username, 'Not a valid username');
+    } else {
+        setSuccessFor(username);
     }
 
-    if(password1Value==='')
-    {
-        showError(password1,"Password can not be blank");
-    }
-    else
-    {
-        showSuccess(password1);
+    if (emailValue === '') {
+        setErrorFor(email, 'Email cannot be empty');
+    } else if (!isEmail(emailValue)) {
+        setErrorFor(email, 'Not a valid email address');
+    } else {
+        setSuccessFor(email);
     }
 
+    if (passwordValue === '') {
+        setErrorFor(password, 'Password cannot be empty');
+    } else {
+        setSuccessFor(password);
+    }
 
-    if(password2Value==='')
-    {
-        showError(password2,"Password can not be blank");
-    }
-    else if(password2Value !==password1Value)
-    {
-        showError(password2,"Password not match");
-    }
-    else
-    {
-        showSuccess(password2);
+    if (password2Value === '') {
+        setErrorFor(password2, 'Confirm Password cannot be empty');
+    } else if (passwordValue !== password2Value) {
+        setErrorFor(password2, 'Passwords does not match');
+    } else {
+        setSuccessFor(password2);
     }
 }
-function showError(input,msg){
-const formControl=input.parentNode;
-formControl.className='form-control error';
-const small=formControl.querySelector('small');
-small.innerHTML=msg;
-}
-function showSuccess(input){
-    const formControl=input.parentNode;
-    formControl.className='form-control success';
 
+function setErrorFor(input, message) {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
 
+function setSuccessFor(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 }
-function isEmailValid(email)
-{
- return /^([a-zA-Z0-9_.\-+]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
+
+function isEmail(email) {
+    return /^([a-zA-Z0-9_.\-+]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,3})$/.test(email);
 }
-function isUsernameValid(username)
-{
-    return /^([a-z]+)$/.test(username);
+
+function isUsernameValid(usernameValue) {
+    return /^([a-z]+)$/.test(usernameValue);
 }
